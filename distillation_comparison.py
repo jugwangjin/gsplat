ccea96924f83acab42c76b67497dfd1f5b0d00d4
Gwangjin Ju <gwangjin@postech.ac.kr>
2025-02-22 18:29:14 +0900

    teacher first

--- distillation_comparison.py
+++ distillation_comparison.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 
-BASE_COMMAND = "CUDA_VISIBLE_DEVICES=1 python student_trainer.py distill2d --teacher_ckpt ../gsplat_teachers/bicycle_4/ckpts/ckpt_29999_rank0.pt --data_factor 4 --data_dir data/360_v2/bicycle/ --disable_viewer --strategy.grow_grad2d 0.0009 --apply_vis_on_teacher_sampling"
+BASE_COMMAND = "CUDA_VISIBLE_DEVICES=1 python student_trainer.py distill2d --teacher_ckpt gsplat_teachers/bicycle_4/ckpts/ckpt_29999_rank0.pt --data_factor 4 --data_dir data/360_v2/bicycle/ --disable_viewer --strategy.grow_grad2d 0.0009 --apply_vis_on_teacher_sampling"
 STUDENT_DIR = "gsplat_students_v7"
 
 def main():
@@ -33,6 +33,7 @@ def main():
 
     commands = []
 
+    commands.append("CUDA_VISIBLE_DEVICES=1 python teacher_trainer.py default --data_factor 4 --data_dir data/360_v2/bicycle --disable_viewer --result_dir gsplat_teachers/bicycle_4")
 
     for combination in combinations:
         distill_sh_lambda = combination[0]
@@ -201,7 +202,6 @@ def main():
             writer.writerow(complete_options)
 
 
-    commands.append("CUDA_VISIBLE_DEVICES=1 python teacher_trainer.py default --data_factor 4 --data_dir data/360_v2/bicycle --disable_viewer --result_dir gsplat_teachers/bicycle_4")
     print(len(commands))
     try:
         for command in commands:
