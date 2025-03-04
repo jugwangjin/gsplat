cb5acedacfd434fa242fb812f98bafa85c99c696
Gwangjin Ju <gwangjin@postech.ac.kr>
2025-03-04 17:19:59 +0900

    collection

--- training_scripts/test_image_loss.py
+++ training_scripts/test_image_loss.py
@@ -50,7 +50,6 @@ def generate_commands():
         {"distill_sh_lambda": 0, "distill_colors_lambda": 0, "distill_depth_lambda": 0, "distill_xyzs_lambda": 0, "distill_quats_lambda": 0, "distill_scales_lambda": 0, "distill_opacities_lambda": 0, "image_lambda": 1},
         {"distill_sh_lambda": 1, "distill_colors_lambda": 1, "distill_depth_lambda": 1, "distill_xyzs_lambda": 1, "distill_quats_lambda": 0, "distill_scales_lambda": 0, "distill_opacities_lambda": 0, "image_lambda": 1},
         {"distill_sh_lambda": 0, "distill_colors_lambda": 1, "distill_depth_lambda": 0, "distill_xyzs_lambda": 0, "distill_quats_lambda": 0, "distill_scales_lambda": 0, "distill_opacities_lambda": 0, "image_lambda": 1},
-        {"distill_sh_lambda": 0, "distill_colors_lambda": 0, "distill_depth_lambda": 0, "distill_xyzs_lambda": 0, "distill_quats_lambda": 0, "distill_scales_lambda": 0, "distill_opacities_lambda": 0, "image_lambda": 0},
         {"distill_sh_lambda": 1, "distill_colors_lambda": 1, "distill_depth_lambda": 1, "distill_xyzs_lambda": 1, "distill_quats_lambda": 0, "distill_scales_lambda": 0, "distill_opacities_lambda": 0, "image_lambda": 0},
         {"distill_sh_lambda": 0, "distill_colors_lambda": 1, "distill_depth_lambda": 0, "distill_xyzs_lambda": 0, "distill_quats_lambda": 0, "distill_scales_lambda": 0, "distill_opacities_lambda": 0, "image_lambda": 0},
     ]
