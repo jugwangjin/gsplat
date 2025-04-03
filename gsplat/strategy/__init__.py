bce8b45069dbb1fe2954b5b6c8f2a062485327a7
Ju Gwangjin <gwangjin@postech.ac.kr>
2025-04-03 07:23:22 +0000

    reorder, visualize ours

--- gsplat/strategy/__init__.py
+++ gsplat/strategy/__init__.py
@@ -4,3 +4,4 @@ from .mcmc import MCMCStrategy
 from .distillation import DistillationStrategy
 from .render_distill import Distill2DStrategy
 from .ms_d import MSDStrategy
+from .image_aware import ImageAwareStrategy
